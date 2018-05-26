6c2f3591a3deb87c0febdf2b697c76f738c638fc
shevuze <shevuze@gmail.com>
2018-05-26 16:20:51 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+;(function($){
+	$(function(){
+		var $fullScreenSlider = $('.ba-slider');
+		$fullScreenSlider.slick({
+			dots: true,
+			infinite: false,
+			arrows: false,
+		});
+
+	})
+
+})(jQuery);
+
+
